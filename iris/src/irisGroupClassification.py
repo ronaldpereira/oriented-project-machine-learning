ad934626d74bedd8b4d0c257b49575444dd83a43
Ronald <ronald.drp11@gmail.com>
2018-07-10 05:01:51 -0300

    added iris group classification

---
+++ iris/src/irisGroupClassification.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import random
+from sklearn.model_selection import train_test_split
+# Imports multi-layer perceptron classifier
+from sklearn.neural_network import MLPClassifier
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# Loads the iris csv dataset
+irisDataset = pd.read_csv('../input/iris_dataset.csv')
+
+# Transform specie string value to int (0 = 'Iris-setosa', 1 = 'Iris-versicolor', 2 = 'Iris-virginica')
+irisDataset.loc[irisDataset['Species'] == 'Iris-setosa', 'Species'] = 0
+irisDataset.loc[irisDataset['Species'] == 'Iris-versicolor', 'Species'] = 1
+irisDataset.loc[irisDataset['Species'] == 'Iris-virginica', 'Species'] = 2
+# Converts all values to numeric
+irisDataset = irisDataset.apply(pd.to_numeric)
+
+# Convert dataframe to matrix
+irisDataset = irisDataset.values
+
+irisDataset = irisDataset.astype('int')
+
+# Splits x and y (features and target)
+x_train, x_test, y_train, y_test = train_test_split(
+    irisDataset[:, :4], irisDataset[:, 4], train_size=0.8)
+
+'''
+Multilayer perceptron model, with one hidden layer.
+input layer : 4 neurons, represents the feature of Iris
+hidden layer : 10 neuron, activation using ReLU
+output layer : 3 neurons, represents the class of Iris
+optimizer = stochastic gradient descent with no batch-size
+loss function = categorical cross entropy
+learning rate = 0.01
+max iterations = 500
+'''
+
+mlp = MLPClassifier(hidden_layer_sizes=10, solver='sgd', learning_rate_init=0.01, max_iter=500)
+
+# Train the model
+mlp.fit(x_train, y_train)
+
+# Test the model
+print(mlp.score(x_test, y_test))
+
+sepalLength = 0.8
+SepalWidth = 0.1
+petalLength = 0.3
+petalWidth = 0.8
+
+data = [[sepalLength, SepalWidth, petalLength, petalWidth]]
+
+print(mlp.predict(x_test))
