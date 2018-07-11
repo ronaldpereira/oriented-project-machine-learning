413d9da3c94104604a724236ba1000aec4e6392b
Ronald <ronald.drp11@gmail.com>
2018-07-11 12:16:16 -0300

    updated code

--- iris/src/irisPUClassification.py
+++ iris/src/irisPUClassification.py
@@ -5,6 +5,7 @@ import random
 from sklearn.model_selection import train_test_split
 # Imports multi-layer perceptron classifier
 from sklearn.neural_network import MLPClassifier
+import PUF1Score
 
 # Ignore warnings
 import warnings
@@ -25,41 +26,44 @@ irisDataset = irisDataset.values
 
 irisDataset = irisDataset.astype('int')
 
-print(irisDataset)
-
 # Splits x and y (features and target)
-positiveQuantity = 60
+positiveQuantity = 20
 x_train, _, y_train, _ = train_test_split(
-    irisDataset[:positiveQuantity, :4], irisDataset[:positiveQuantity, 4], train_size=0.99)
+    irisDataset[:positiveQuantity, :4], irisDataset[:positiveQuantity, 4], train_size=positiveQuantity-1)
 
 # Splits x and y (features and target)
 _, x_test, _, y_test = train_test_split(
-    irisDataset[positiveQuantity:, :4], irisDataset[positiveQuantity:, 4], test_size=0.99)
+    irisDataset[positiveQuantity:, :4], irisDataset[positiveQuantity:, 4], test_size=150-positiveQuantity-1)
 
 '''
 Multilayer perceptron model, with one hidden layer.
 input layer : 4 neurons, represents the feature of Iris
 hidden layer : 10 neuron, activation using ReLU
-output layer : 3 neurons, represents the class of Iris
+output layer : 2 neurons, represents the positive or negative class of Iris
 optimizer = stochastic gradient descent with no batch-size
 loss function = categorical cross entropy
 learning rate = 0.01
 max iterations = 500
 '''
 
-mlp = MLPClassifier(hidden_layer_sizes=10, solver='sgd', learning_rate_init=0.01, max_iter=500)
+score = 0.0
+while score < 0.8:
+    mlp = MLPClassifier(hidden_layer_sizes=10, solver='sgd', learning_rate_init=0.01, max_iter=500)
 
-# Train the model
-mlp.fit(x_train, y_train)
+    # Train the model
+    mlp.fit(x_train, y_train)
+
+    score = PUF1Score.calculateF1Score(mlp.predict(x_test), y_test)
+    print(score)
 
 # Test the model
-print(mlp.score(x_test, y_test))
+print(score)
 
 sepalLength = 0.8
 SepalWidth = 0.1
 petalLength = 0.3
 petalWidth = 0.8
 
-data = [[sepalLength, SepalWidth, petalLength, petalWidth]]
+customData = [[sepalLength, SepalWidth, petalLength, petalWidth]]
 
-print(mlp.predict(x_test))
+print(mlp.predict(customData))
